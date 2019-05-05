a5451c11e137d6f9ec3e5deca0600bb770f50154
fufic <fufic1@yandex.ru>
2019-05-05 18:37:44 +0300

    config

--- zero_messeger/zero_messeger.py
+++ zero_messeger/zero_messeger.py
@@ -7,7 +7,7 @@ class Sender:
         self.sock = self.context.socket(zmq.REQ)
         self.sock.bind(address)
         
-    def send(self, message = "Hi"):   
+    def Send(self, message = "Hi"):   
         self.sock.send(message)
         response = self.sock.recv()
         print(response.decode())
@@ -30,10 +30,34 @@ class Receiver:
             response = self.handle_message(message)
             self.sock.send(response)
 
+class Publisher:
+    
+    def __init__(self, address = 'tcp://*:8080'):
+        self.context = zmq.Context.instance()
+        self.sock = self.context.socket(zmq.PUB)
+        self.sock.bind(address)
+        
+    def Send(self, message ):   
+        self.sock.send(message.encode())
+        print(str(message) + " sent")
+
+class Subscriber:
+    def __init__(self, address = 'tcp://127.0.0.1:8080'):
+        self.context = zmq.Context.instance()
+        self.sock = self.context.socket(zmq.SUB)
+        self.sock.setsockopt(zmq.SUBSCRIBE, b"");
+        self.sock.connect(address)
+          
+    def GetUpdates(self ):
+        while True:
+            message = self.sock.recv()
+            print(message.decode() + "recieved")
 
 if __name__ == "__main__":
     server = Receiver()
     message = "Test"
     response = server.handle_message(message)
     print(response)
+    
+
     
